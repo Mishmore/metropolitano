831496508297237bfad4ba219307e381fe533386
Katherine Ortega <25912528+katherineortega@users.noreply.github.com>
2018-03-24 14:51:23 -0500

    change server

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const app = express();
+const json = require('./json/districts.json');
+let app = express();
 
-app.use(express.static(__dirname + '/dist'));
-
-const path = require('path');
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || 8080);
+
+const districts = json.districts;
+
+app.get('/districts', function (req, res) {
+  res.json(districts);
+});
+
+app.get('/districts/:id', function (req, res) {
+  const id = parseInt(req.params.id, 10);
+  const result = districts.filter(r => r.id === id)[0];
+
+  if (!result) {
+    res.sendStatus(404);
+  } else {
+    res.send(result);
+  }
+});
+
+
+const server = app.listen(3000, function () {
+  let host = server.address().address;
+  host = (host === '::' ? 'localhost' : host);
+  const port = server.address().port;
+
+  console.log('listening at http://%s:%s', host, port);
+});
